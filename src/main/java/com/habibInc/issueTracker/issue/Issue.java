b2ceaf4e0f1a72bcf55d7bba7facd45c2fcf6401
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 15:29:39 +0100

    Fix: integration test

--- src/main/java/com/habibInc/issueTracker/issue/Issue.java
+++ src/main/java/com/habibInc/issueTracker/issue/Issue.java
@@ -1,8 +1,19 @@
-package com.habibInc.issueTracker.Issue;
+package com.habibInc.issueTracker.issue;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
 public class Issue {
 
-    private final Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Issue(){}
 
     public Issue(Long id) {
         this.id = id;
--- src/test/java/com/habibInc/issueTracker/issue/IssueIT.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueIT.java
@@ -1,23 +1,21 @@
-package com.habibInc.issueTracker.Issue;
+package com.habibInc.issueTracker.issue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.*;
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IssueIT {
     @Autowired
-    TestRestTemplate template;
+    TestRestTemplate restTemplate;
 
     @Test
-    public void itShouldReturnIssue(){
-        ResponseEntity<Issue> response = template.getForEntity("/issues/1", Issue.class);
+    public void itShouldReturnIssue() throws Exception{
+        ResponseEntity<Issue> response = restTemplate.getForEntity("/issues/1", Issue.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody().getId()).isEqualTo(1);
     }
