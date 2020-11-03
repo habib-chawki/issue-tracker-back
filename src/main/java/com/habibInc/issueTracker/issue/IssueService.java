0eb62674823c7275e95ab57a08999082b44f68a9
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 17:29:50 +0100

    Add create issue

--- src/main/java/com/habibInc/issueTracker/issue/IssueService.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueService.java
@@ -1,10 +1,15 @@
 package com.habibInc.issueTracker.issue;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IssueService {
     public Issue getIssue(Long id) {
+        return null;
+    }
+
+    public ResponseEntity<Issue> createIssue(Issue issue) {
         return null;
     }
 }
