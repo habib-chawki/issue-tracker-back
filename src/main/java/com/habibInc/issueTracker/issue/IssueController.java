fc7268c16f32dfb1e7f11285c15d9a728e1a46fe
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 16:37:44 +0100

    Add create issue post mapping

--- src/main/java/com/habibInc/issueTracker/issue/IssueController.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueController.java
@@ -1,5 +1,7 @@
 package com.habibInc.issueTracker.issue;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,5 +11,10 @@ public class IssueController {
     @GetMapping("/{id}")
     public Issue getIssue(@PathVariable Long id){
         return new Issue(1L);
+    }
+
+    @PostMapping({"", "/"})
+    public ResponseEntity<Issue> createIssue(){
+        return new ResponseEntity<Issue>(new Issue(2L), HttpStatus.CREATED);
     }
 }
