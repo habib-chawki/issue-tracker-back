71de39e363afb8be9923932c4b00e3e2426c60bd
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 16:49:36 +0100

    Mock issue service

--- src/test/java/com/habibInc/issueTracker/issue/IssueControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueControllerTest.java
@@ -3,9 +3,12 @@ package com.habibInc.issueTracker.issue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -15,8 +18,14 @@ public class IssueControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    IssueService issueService;
+
     @Test
     public void itShouldGetIssueById() throws Exception {
+        // mock issue service and return new issue
+        when(issueService.getIssue(anyLong())).thenReturn(new Issue(1L));
+
         mockMvc.perform(get("/issues/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
