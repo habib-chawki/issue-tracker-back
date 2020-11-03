e61d3d2680d0a66891efb4b360cb8976d3b41822
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 16:48:32 +0100

    Add issue service

---
+++ src/main/java/com/habibInc/issueTracker/issue/IssueService.java
@@ -0,0 +1,10 @@
+package com.habibInc.issueTracker.issue;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IssueService {
+    public Issue getIssue(Long id) {
+        return null;
+    }
+}
