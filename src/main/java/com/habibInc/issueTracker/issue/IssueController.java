2a1897f91bac0f14f65db3fcc1bc3bd9c6ae17a1
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 16:48:59 +0100

    Autowire issue service

--- src/main/java/com/habibInc/issueTracker/issue/IssueController.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueController.java
@@ -1,5 +1,6 @@
 package com.habibInc.issueTracker.issue;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/issues")
 public class IssueController {
+    @Autowired
+    IssueService issueService;
 
     @GetMapping("/{id}")
     public Issue getIssue(@PathVariable Long id){
-        return new Issue(1L);
+        return issueService.getIssue(id);
     }
 
     @PostMapping({"", "/"})
