caead85432a6d7876b2de0ac0acd725ee4a7a027
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 12:09:29 +0100

    Add Issue entity

---
+++ src/main/java/com/habibInc/issueTracker/Issue/Issue.java
@@ -0,0 +1,14 @@
+package com.habibInc.issueTracker.Issue;
+
+public class Issue {
+
+    private final Long id;
+
+    public Issue(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+}
