b501d42fc6c45a352efea868998235f1efaa32f9
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-03 16:38:15 +0100

    Test create new issue

--- src/test/java/com/habibInc/issueTracker/issue/IssueControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(IssueController.class)
@@ -20,5 +21,12 @@ public class IssueControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("id").value("1"));
+    }
+
+    @Test
+    public void itShouldCreateIssue() throws Exception {
+        mockMvc.perform(post("/issues"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
