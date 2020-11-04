93e514d07fc6303bb0cd166045dbaa7bd39fdec5
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-04 14:30:50 +0100

    Fix: IssueService#getIssue

--- src/main/java/com/habibInc/issueTracker/issue/IssueService.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueService.java
@@ -1,11 +1,15 @@
 package com.habibInc.issueTracker.issue;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IssueService {
+    @Autowired
+    IssueRepository issueRepository;
+
     public Issue getIssue(Long id) {
-        return null;
+        return issueRepository.findById(id).get();
     }
 
     public Issue createIssue(Issue issue) {
