96894c65b7dc0af8d49e50970b4b4c41bbc82826
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-04 14:29:44 +0100

    Test issue service with mocked repository

--- src/test/java/com/habibInc/issueTracker/issue/IssueServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueServiceTest.java
@@ -1,4 +1,40 @@
 package com.habibInc.issueTracker.issue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.*;
+
 public class IssueServiceTest {
+
+    // inject mocks into issue service
+    @InjectMocks
+    IssueService issueService;
+
+    // create repository mock
+    @Mock
+    IssueRepository issueRepository;
+
+    @BeforeEach
+    public void setup(){
+        // initialize mocks
+        initMocks(this);
+    }
+
+    @Test
+    public void itShouldGetIssueById(){
+        // mock repository behaviour and return an issue optional
+        Issue issue = new Issue(1L);
+        when(issueRepository.findById(anyLong())).thenReturn(Optional.of(issue));
+
+        Issue returnedIssue = issueService.getIssue(1L);
+
+        assertThat(returnedIssue.getId()).isEqualTo(1L);
+    }
 }
