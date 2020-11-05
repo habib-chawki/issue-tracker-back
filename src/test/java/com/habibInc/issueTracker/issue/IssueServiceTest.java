f49108dad68a2917fb103574ddfcbbe467c62721
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-05 15:06:42 +0100

    Test get all issues

--- src/test/java/com/habibInc/issueTracker/issue/IssueServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -33,8 +36,10 @@ public class IssueServiceTest {
         Issue issue = new Issue(1L);
         when(issueRepository.findById(anyLong())).thenReturn(Optional.of(issue));
 
+        // get the issue by id
         Issue returnedIssue = issueService.getIssue(1L);
 
+        // expect the proper issue to have been retrieved
         assertThat(returnedIssue.getId()).isEqualTo(1L);
     }
 
@@ -44,8 +49,27 @@ public class IssueServiceTest {
         Issue issue = new Issue(1L);
         when(issueRepository.save(any(Issue.class))).thenReturn(issue);
 
+        // create the issue
         Issue returnedIssue = issueService.createIssue(issue);
 
+        // expect the issue to have been created successfully
         assertThat(returnedIssue.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void itShouldGetAllIssues(){
+        // create a list of mocked issues
+        Issue issue1 = new Issue();
+        Issue issue2 = new Issue();
+        List<Issue> mockedIssues = Arrays.asList(issue1, issue2);
+
+        when(issueRepository.findAll()).thenReturn(mockedIssues);
+
+        // retrieve list of issues
+        Iterable<Issue> issues = issueService.getIssues();
+
+        // expect all issues to have been retrieved successfully
+        assertThat(issues).contains(issue1);
+        assertThat(issues).contains(issue2);
     }
 }
