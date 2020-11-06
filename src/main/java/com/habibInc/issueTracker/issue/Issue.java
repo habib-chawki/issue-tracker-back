3a3d75c21127936b88ad6ee95f4642e86819fc8c
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-06 15:43:49 +0100

    Add issue properties

--- src/main/java/com/habibInc/issueTracker/issue/Issue.java
+++ src/main/java/com/habibInc/issueTracker/issue/Issue.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -13,14 +17,133 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String key;
+
+    private String description;
+    private String summary;
+
+    private IssueType type;
+    private IssueResolution resolution;
+
+    private List<String> comments;
+    private int votes;
+
+    private String assignee;
+    private String reporter;
+
+    private LocalDateTime creationTime;
+    private LocalDateTime updateTime;
+    private LocalTime estimate;
+
+
     public Issue(){}
 
     public Issue(Long id) {
         this.id = id;
     }
 
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return this.id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getSummary(){
+        return summary;
+    }
+
+    public void setType(IssueType type) {
+        this.type = type;
+    }
+
+    public IssueType getType() {
+        return type;
+    }
+
+    public void setResolution(IssueResolution resolution) {
+        this.resolution = resolution;
+    }
+
+    public IssueResolution getResolution() {
+        return resolution;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setEstimate(LocalTime estimate) {
+        this.estimate = estimate;
+    }
+
+    public LocalTime getEstimate() {
+        return estimate;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
     }
 
     @Override
