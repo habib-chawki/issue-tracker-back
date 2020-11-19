f25fa346c546a380f271525a4e021ce1a2993eb7
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-19 11:48:44 +0100

    Add setup method

--- src/test/java/com/habibInc/issueTracker/comment/CommentControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/comment/CommentControllerTest.java
@@ -1,12 +1,25 @@
 package com.habibInc.issueTracker.comment;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(CommentController.class)
 public class CommentControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        Comment comment = new Comment();
+    }
+
     @Test
     public void itShouldCreateComment(){
-
     }
 }
