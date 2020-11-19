8f28fa6a187750e4f581ef85840b5e0b867e0f02
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-19 11:38:28 +0100

    Add comment controller test

---
+++ src/test/java/com/habibInc/issueTracker/comment/CommentControllerTest.java
@@ -0,0 +1,12 @@
+package com.habibInc.issueTracker.comment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(CommentController.class)
+public class CommentControllerTest {
+    @Test
+    public void itShouldCreateComment(){
+
+    }
+}
