9a769d69b80f2ffff1c867bb8391eba8b22d721f
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-19 12:11:15 +0100

    Add owner, creationTime and updateTime 'Comment' properties

--- src/main/java/com/habibInc/issueTracker/comment/Comment.java
+++ src/main/java/com/habibInc/issueTracker/comment/Comment.java
@@ -1,4 +1,33 @@
 package com.habibInc.issueTracker.comment;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+    private String owner;
+    private LocalDateTime creationTime;
+    private LocalDateTime updateTime;
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
 }
