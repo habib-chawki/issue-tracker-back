6026d99ee6daaac6fa533fed3f41f7dd87f53ab8
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-19 13:53:11 +0100

    Implement createComment post request handler method

--- src/main/java/com/habibInc/issueTracker/comment/CommentController.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentController.java
@@ -1,4 +1,23 @@
 package com.habibInc.issueTracker.comment;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping({"", "/"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(Comment comment){
+        return commentService.createComment(comment);
+    }
 }
