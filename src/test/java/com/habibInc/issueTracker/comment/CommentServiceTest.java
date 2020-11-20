a9ea1741ccd298aff51ca7c9c89362625d001f80
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-20 15:08:15 +0100

    Initialize mocks

--- src/test/java/com/habibInc/issueTracker/comment/CommentServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/comment/CommentServiceTest.java
@@ -14,19 +14,25 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 public class CommentServiceTest {
 
+    // inject mocks into the service
     @InjectMocks
     CommentService commentService;
 
+    // mock the repository
     @Mock
     CommentRepository commentRepository;
 
     Comment comment;
 
     @BeforeEach
-    public void setup(){
+    public void init() {
         // initialize mocks
         initMocks(this);
+    }
 
+    @BeforeEach
+    public void setup() {
+        // set up a new comment
         comment = new Comment();
 
         comment.setContent("My comment");
@@ -36,13 +42,15 @@ public class CommentServiceTest {
         comment.setUpdateTime(LocalDateTime.now());
     }
 
-
     @Test
-    public void itShouldCreateComment(){
+    public void itShouldCreateComment() {
+        // given a call to the repository "save()" method
         when(commentRepository.save(any(Comment.class))).thenReturn(comment);
 
+        // when the "createComment()" service method is called
         Comment response = commentService.createComment(comment);
 
+        // then the response should be the comment itself
         assertThat(response).isEqualTo(comment);
     }
 }
