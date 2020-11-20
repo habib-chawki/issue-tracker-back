4b1f2b427c5a0e1ff1cb7c09ad9edfce0c151516
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-20 14:52:01 +0100

    Implement create comment service method

--- src/main/java/com/habibInc/issueTracker/comment/CommentService.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentService.java
@@ -1,10 +1,18 @@
 package com.habibInc.issueTracker.comment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    private CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository){
+        this.commentRepository = commentRepository;
+    }
+
     public Comment createComment(Comment comment) {
-        return null;
+        return commentRepository.save(comment);
     }
 }
