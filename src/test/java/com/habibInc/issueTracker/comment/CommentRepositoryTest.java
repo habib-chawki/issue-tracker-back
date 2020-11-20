3f542a9a44f692ed04dd1c4aa92c1dbfa58ef85b
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-20 16:15:13 +0100

    Test commentRepository#save

--- src/test/java/com/habibInc/issueTracker/comment/CommentRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/comment/CommentRepositoryTest.java
@@ -1,15 +1,19 @@
 package com.habibInc.issueTracker.comment;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CommentRepositoryTest {
+
     @Autowired
     CommentRepository commentRepository;
 
@@ -20,12 +24,18 @@ public class CommentRepositoryTest {
         // set up a new comment
         comment = new Comment();
 
-        comment.setId(1L);
-
         comment.setContent("My comment");
         comment.setOwner("Me");
 
         comment.setCreationTime(LocalDateTime.now());
         comment.setUpdateTime(LocalDateTime.now());
+    }
+
+    @Test
+    public void itShouldSaveComment(){
+        Comment response = commentRepository.save(comment);
+
+        assertThat(response).isEqualTo(comment);
+        assertThat(response.getId()).isNotNull().isPositive();
     }
 }
