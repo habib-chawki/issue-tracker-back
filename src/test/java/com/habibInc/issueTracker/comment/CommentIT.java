b2bf5a652512829b2fc40b709dd55dd46e944266
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-20 16:36:20 +0100

    Refactor: remove unecessary import statements

--- src/test/java/com/habibInc/issueTracker/comment/CommentIT.java
+++ src/test/java/com/habibInc/issueTracker/comment/CommentIT.java
@@ -1,6 +1,5 @@
 package com.habibInc.issueTracker.comment;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/habibInc/issueTracker/issue/IssueIT.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueIT.java
@@ -1,6 +1,5 @@
 package com.habibInc.issueTracker.issue;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
