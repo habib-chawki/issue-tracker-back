25d5c31553992ebd19275b487452a915be07aa4a
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-23 16:10:24 +0100

    Use Lombok annotations

--- src/main/java/com/habibInc/issueTracker/exceptionhandler/ApiError.java
+++ src/main/java/com/habibInc/issueTracker/exceptionhandler/ApiError.java
@@ -1,42 +1,22 @@
 package com.habibInc.issueTracker.exceptionhandler;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class ApiError {
     private String errorMessage;
     private HttpStatus status;
     private LocalDateTime timestamp;
 
-    public ApiError() {}
-
     public ApiError(String errorMessage, LocalDateTime timestamp) {
         this.errorMessage = errorMessage;
-        this.timestamp = timestamp;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
     }
 }
