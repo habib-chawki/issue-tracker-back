0452bd5785ea716d30c39fa500d9e9f53652735e
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-23 16:10:11 +0100

    Use Lombok annotations

--- src/main/java/com/habibInc/issueTracker/issue/Issue.java
+++ src/main/java/com/habibInc/issueTracker/issue/Issue.java
@@ -1,6 +1,10 @@
 package com.habibInc.issueTracker.issue;
 
 import com.habibInc.issueTracker.comment.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.NaturalId;
@@ -12,6 +16,10 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Issue {
 
     @Id
@@ -39,135 +47,6 @@ public class Issue {
     private LocalDateTime creationTime;
     private LocalDateTime updateTime;
     private LocalTime estimate;
-
-    // constructors
-    public Issue(){}
-
-    public Issue(Long id, String IssueKey, String description, String summary,
-                 IssueType type, IssueResolution resolution,
-                 List<Comment> comments, int votes,
-                 String assignee, String reporter,
-                 LocalDateTime creationTime, LocalDateTime updateTime,
-                 LocalTime estimate) {
-        this.id = id;
-        this.IssueKey = IssueKey;
-        this.description = description;
-        this.summary = summary;
-        this.type = type;
-        this.resolution = resolution;
-        this.comments = comments;
-        this.votes = votes;
-        this.assignee = assignee;
-        this.reporter = reporter;
-        this.creationTime = creationTime;
-        this.updateTime = updateTime;
-        this.estimate = estimate;
-    }
-
-    // access methods
-    public void setIssueKey(String issueKey) {
-        this.IssueKey = issueKey;
-    }
-
-    public String getIssueKey() {
-        return IssueKey;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getSummary(){
-        return summary;
-    }
-
-    public void setType(IssueType type) {
-        this.type = type;
-    }
-
-    public IssueType getType() {
-        return type;
-    }
-
-    public void setResolution(IssueResolution resolution) {
-        this.resolution = resolution;
-    }
-
-    public IssueResolution getResolution() {
-        return resolution;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setVotes(int votes) {
-        this.votes = votes;
-    }
-
-    public int getVotes() {
-        return votes;
-    }
-
-    public void setAssignee(String assignee) {
-        this.assignee = assignee;
-    }
-
-    public String getAssignee() {
-        return assignee;
-    }
-
-    public void setReporter(String reporter) {
-        this.reporter = reporter;
-    }
-
-    public String getReporter() {
-        return reporter;
-    }
-
-    public void setEstimate(LocalTime estimate) {
-        this.estimate = estimate;
-    }
-
-    public LocalTime getEstimate() {
-        return estimate;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setCreationTime(LocalDateTime creationTime) {
-        this.creationTime = creationTime;
-    }
-
-    public LocalDateTime getCreationTime() {
-        return creationTime;
-    }
 
     // equals and hashCode
     @Override
