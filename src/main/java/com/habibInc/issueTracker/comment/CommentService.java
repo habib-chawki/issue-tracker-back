b81921e037ab593fea5466be5f92d787c2f4ede6
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-23 10:55:40 +0100

    Handle incorrect issue id error

--- src/main/java/com/habibInc/issueTracker/comment/CommentService.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentService.java
@@ -1,18 +1,28 @@
 package com.habibInc.issueTracker.comment;
 
+import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
+import com.habibInc.issueTracker.issue.Issue;
+import com.habibInc.issueTracker.issue.IssueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    private IssueService issueService;
     private CommentRepository commentRepository;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository){
+    public CommentService(IssueService issueService, CommentRepository commentRepository){
+        this.issueService = issueService;
         this.commentRepository = commentRepository;
     }
 
     public Comment createComment(Comment comment) {
-        return commentRepository.save(comment);
+        Issue issue = issueService.getIssue(comment.getIssue().getId());
+
+        if(issue != null)
+            return commentRepository.save(comment);
+
+        throw new ResourceNotFoundException("Issue not found");
     }
 }
