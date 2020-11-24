350037c931a7e1dd158479c7eb2499b0684235c7
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-24 15:39:15 +0100

    Remove ResponseEntityExceptionHandler

--- src/main/java/com/habibInc/issueTracker/exceptionhandler/RestExceptionHandler.java
+++ src/main/java/com/habibInc/issueTracker/exceptionhandler/RestExceptionHandler.java
@@ -4,12 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex){
