1f916b0fe4749ade2042a3e18cbfd38837c5e872
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-24 15:16:44 +0100

    Add invalid id exception

---
+++ src/main/java/com/habibInc/issueTracker/exceptionhandler/InvalidIdException.java
@@ -0,0 +1,11 @@
+package com.habibInc.issueTracker.exceptionhandler;
+
+public class InvalidIdException extends RuntimeException {
+    public InvalidIdException(String message) {
+        super(message);
+    }
+
+    public InvalidIdException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
