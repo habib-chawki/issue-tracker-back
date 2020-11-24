8bcd78ed463c251753f15f726dafe8d50e14b538
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-24 15:22:46 +0100

    Handle invalid issue id exception

--- src/main/java/com/habibInc/issueTracker/comment/CommentController.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentController.java
@@ -1,18 +1,24 @@
 package com.habibInc.issueTracker.comment;
 
+import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/issues/{id}/comments")
 public class CommentController {
     @Autowired
     CommentService commentService;
 
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
-    public Comment createComment(@RequestBody Comment comment){
-        return commentService.createComment(comment);
+    public Comment createComment(@RequestBody Comment comment, @PathVariable String id){
+        try{
+            Long issueId = Long.parseLong(id);
+            return commentService.createComment(comment);
+        }catch(NumberFormatException ex){
+            throw new InvalidIdException("Invalid issue id");
+        }
     }
 }
