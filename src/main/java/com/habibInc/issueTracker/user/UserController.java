7b5f44aa372857127fdf5ec3b5298d193eef9e95
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-25 16:22:59 +0100

    Implement create user handler

--- src/main/java/com/habibInc/issueTracker/user/UserController.java
+++ src/main/java/com/habibInc/issueTracker/user/UserController.java
@@ -1,16 +1,19 @@
 package com.habibInc.issueTracker.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
-    public void createUser() {}
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
 }
