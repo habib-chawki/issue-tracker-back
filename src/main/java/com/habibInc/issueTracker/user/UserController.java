575694f7f87dec754f3566fdcf35407f15ad894a
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-25 15:44:45 +0100

    Add create user post request handler

--- src/main/java/com/habibInc/issueTracker/user/UserController.java
+++ src/main/java/com/habibInc/issueTracker/user/UserController.java
@@ -1,7 +1,16 @@
 package com.habibInc.issueTracker.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping({"", "/"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser() {}
 }
--- src/test/java/com/habibInc/issueTracker/user/UserControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserControllerTest.java
@@ -1,12 +1,21 @@
 package com.habibInc.issueTracker.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
-    @Test
-    public void itShouldCreateUser() {
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    public void itShouldCreateUser() throws Exception {
+        mockMvc.perform(post("/users"))
+                .andExpect(status().isCreated());
     }
 }
