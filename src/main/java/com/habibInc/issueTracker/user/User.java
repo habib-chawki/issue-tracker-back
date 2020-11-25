344db80612939a883174cf37498940ce0284e452
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-25 16:22:19 +0100

    Add user entity

---
+++ src/main/java/com/habibInc/issueTracker/user/User.java
@@ -0,0 +1,16 @@
+package com.habibInc.issueTracker.user;
+
+public class User {
+    private String email;
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
