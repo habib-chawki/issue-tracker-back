c8fe134eb791a076b3af8dba429450371a4d4c83
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-25 16:22:32 +0100

    Add user service

---
+++ src/main/java/com/habibInc/issueTracker/user/UserService.java
@@ -0,0 +1,10 @@
+package com.habibInc.issueTracker.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public User createUser(User user) {
+        return null;
+    }
+}
