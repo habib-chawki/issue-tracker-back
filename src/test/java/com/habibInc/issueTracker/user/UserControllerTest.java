1b427f122095f314ae5cd2360a23c75f53b4647b
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-25 16:03:11 +0100

    Fix create user test, unable to load context error, use @MockBean instead of @Autowired

--- src/test/java/com/habibInc/issueTracker/user/UserControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserControllerTest.java
@@ -1,10 +1,14 @@
 package com.habibInc.issueTracker.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,9 +17,20 @@ public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    UserService userService;
+
+    User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+    }
+
     @Test
     public void itShouldCreateUser() throws Exception {
-        mockMvc.perform(post("/users"))
+        mockMvc.perform(post("/users", user)
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
 }
