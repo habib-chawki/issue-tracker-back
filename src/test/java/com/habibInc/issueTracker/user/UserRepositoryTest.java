58820d58f36a6cd705206880f65b10d0c81097a8
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 12:16:06 +0100

    Test UserRepository#save

--- src/test/java/com/habibInc/issueTracker/user/UserRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserRepositoryTest.java
@@ -1,11 +1,38 @@
 package com.habibInc.issueTracker.user;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+
+        user.setFirstName("first");
+        user.setLastName("last");
+        user.setUserName("my_username");
+        user.setEmail("my_email@email.com");
+        user.setPassword("this is it");
+    }
+
+    @Test
+    public void itShouldSaveUser() {
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).isEqualToComparingOnlyGivenFields(user);
+    }
 }
