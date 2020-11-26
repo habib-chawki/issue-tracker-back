5a62a7ebadd18fd2be301795edbf2bd296ef5b85
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 11:38:46 +0100

    Init mocks for user service tests

--- src/test/java/com/habibInc/issueTracker/user/UserServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserServiceTest.java
@@ -2,19 +2,28 @@ package com.habibInc.issueTracker.user;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class UserServiceTest {
-    @Autowired
+    @InjectMocks
     UserService userService;
 
-    @MockBean
+    @Mock
     UserRepository userRepository;
 
     User user;
+
+    @BeforeEach
+    public void init() {
+        initMocks(this);
+    }
 
     @BeforeEach
     public void setup() {
@@ -24,6 +33,6 @@ public class UserServiceTest {
 
     @Test
     public void itShouldCreateUser() {
-//        when(userRepository.save(user)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
     }
 }
