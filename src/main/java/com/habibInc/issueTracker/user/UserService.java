09aa18b59d6e4fd9151154ed1cb22f8d0a4dea87
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 11:36:21 +0100

    Inject user repository into user service

--- src/main/java/com/habibInc/issueTracker/user/UserService.java
+++ src/main/java/com/habibInc/issueTracker/user/UserService.java
@@ -1,10 +1,18 @@
 package com.habibInc.issueTracker.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public User createUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 }
