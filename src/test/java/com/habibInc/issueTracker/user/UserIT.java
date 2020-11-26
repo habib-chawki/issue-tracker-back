b93cd1962a60152b7453e97f7b2e50c6ce2166c6
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 12:29:25 +0100

    Add @SpringBootTest annotation with webEnvironment random port

--- src/test/java/com/habibInc/issueTracker/user/UserIT.java
+++ src/test/java/com/habibInc/issueTracker/user/UserIT.java
@@ -1,10 +1,22 @@
 package com.habibInc.issueTracker.user;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserIT {
+    @Autowired
+    TestRestTemplate restTemplate;
+
     @Autowired
     UserRepository userRepository;
 
@@ -17,12 +29,19 @@ public class UserIT {
     public void setup() {
         user = new User();
 
-        user.setId(1L);
         user.setFirstName("first");
         user.setLastName("last");
         user.setUserName("my_username");
         user.setEmail("my_email@email.com");
         user.setPassword("this is it");
+    }
+
+    @Test
+    public void itShouldCreateUser() {
+        ResponseEntity<User> response =
+                restTemplate.postForEntity("/users", user, User.class);
+
+        assertThat(response.getBody()).isEqualToComparingOnlyGivenFields(user);
     }
 
     @AfterEach
