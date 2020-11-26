3d0e250c07eab2220ad1a28bb5dcf84b74bdb65f
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 12:22:43 +0100

    Autowire user service and repository and add a teardown method for UserIT

--- src/test/java/com/habibInc/issueTracker/user/UserIT.java
+++ src/test/java/com/habibInc/issueTracker/user/UserIT.java
@@ -1,4 +1,32 @@
 package com.habibInc.issueTracker.user;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserIT {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+
+        user.setId(1L);
+        user.setFirstName("first");
+        user.setLastName("last");
+        user.setUserName("my_username");
+        user.setEmail("my_email@email.com");
+        user.setPassword("this is it");
+    }
+
+    @AfterEach
+    public void teardown() {
+        userRepository.deleteAll();
+    }
 }
