94842ed4654b557b317fd1faa43881ffecd282ef
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 11:35:31 +0100

    Test user service: create user

--- src/test/java/com/habibInc/issueTracker/user/UserServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserServiceTest.java
@@ -1,4 +1,29 @@
 package com.habibInc.issueTracker.user;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.Mockito.when;
+
 public class UserServiceTest {
+    @Autowired
+    UserService userService;
+
+    @MockBean
+    UserRepository userRepository;
+
+    User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+        user.setEmail("my_email@email.com");
+    }
+
+    @Test
+    public void itShouldCreateUser() {
+//        when(userRepository.save(user)).thenReturn(user);
+    }
 }
