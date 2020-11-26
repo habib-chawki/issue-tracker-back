2759d8e2ddf40e9cf41797dd0c5335ac47530192
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-26 11:52:57 +0100

    Add properties to user entity

--- src/main/java/com/habibInc/issueTracker/user/User.java
+++ src/main/java/com/habibInc/issueTracker/user/User.java
@@ -2,6 +2,11 @@ package com.habibInc.issueTracker.user;
 
 public class User {
     private String email;
+    private Long id;
+    private String password;
+    private String userName;
+    private String firstName;
+    private String lastName;
 
     public User() {
     }
@@ -16,5 +21,45 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
