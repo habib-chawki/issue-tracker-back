fffcf324d1fc3dcae370909c303d794a02f4ec8d
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-27 14:57:07 +0100

    Remove users from issue's unit tests

--- src/test/java/com/habibInc/issueTracker/issue/IssueServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueServiceTest.java
@@ -1,7 +1,6 @@
 package com.habibInc.issueTracker.issue;
 
 import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
-import com.habibInc.issueTracker.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -28,7 +27,6 @@ public class IssueServiceTest {
     @Mock
     IssueRepository issueRepository;
 
-    User reporter, assignee1, assignee2;
     Issue issue1, issue2;
 
     @BeforeEach
@@ -43,17 +41,11 @@ public class IssueServiceTest {
         issue1 = new Issue();
         issue2 = new Issue();
 
-        reporter = new User();
-        assignee1 = new User();
-        assignee2 = new User();
-
         // set up issue1 properties
         issue1.setSummary("Issue 1 summary");
         issue1.setDescription("Issue 1 description");
         issue1.setType(IssueType.STORY);
         issue1.setResolution(IssueResolution.DONE);
-        issue1.setAssignee(assignee1);
-        issue1.setReporter(reporter);
         issue1.setCreationTime(LocalDateTime.now());
         issue1.setUpdateTime(LocalDateTime.now());
         issue1.setEstimate(LocalTime.of(2, 0));
@@ -63,8 +55,6 @@ public class IssueServiceTest {
         issue2.setDescription("Issue 2 description");
         issue2.setType(IssueType.TASK);
         issue2.setResolution(IssueResolution.DUPLICATE);
-        issue2.setAssignee(assignee2);
-        issue2.setReporter(reporter);
         issue2.setCreationTime(LocalDateTime.now());
         issue2.setUpdateTime(LocalDateTime.now());
         issue2.setEstimate(LocalTime.of(6, 15));
