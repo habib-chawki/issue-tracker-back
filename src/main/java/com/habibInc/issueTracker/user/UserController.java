45cb0294b4822882dd7a0384e511ed30627e210c
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-28 11:27:16 +0100

    Handle invalid user id exception

--- src/main/java/com/habibInc/issueTracker/user/UserController.java
+++ src/main/java/com/habibInc/issueTracker/user/UserController.java
@@ -1,5 +1,6 @@
 package com.habibInc.issueTracker.user;
 
+import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,13 @@ public class UserController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public User getUser(@PathVariable Long id){
-        return userService.getUser(id);
+    public User getUser(@PathVariable String id){
+        try{
+            Long userId = Long.parseLong(id);
+            return userService.getUser(userId);
+        }catch(NumberFormatException ex){
+            throw new InvalidIdException("Invalid user id");
+        }
+
     }
 }
