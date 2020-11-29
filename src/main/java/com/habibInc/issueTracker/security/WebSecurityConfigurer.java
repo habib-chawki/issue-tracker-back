2a02c519cc43991162682c9c599b3094ea881845
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-29 12:22:05 +0100

    Enable web security and disable csrf

--- src/main/java/com/habibInc/issueTracker/security/WebSecurityConfigurer.java
+++
@@ -1,8 +0,0 @@
-package com.habibInc.issueTracker.security;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-
-}
