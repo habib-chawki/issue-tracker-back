ff299860f1d4801002e6e6d06f2cfa18f64aace0
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-29 12:34:42 +0100

    Autowire bcrypt password encoder

--- src/main/java/com/habibInc/issueTracker/user/UserService.java
+++ src/main/java/com/habibInc/issueTracker/user/UserService.java
@@ -2,16 +2,19 @@ package com.habibInc.issueTracker.user;
 
 import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public User createUser(User user) {
