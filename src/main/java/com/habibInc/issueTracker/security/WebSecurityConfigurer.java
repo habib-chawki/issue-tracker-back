868a9067bc64265b408bd2c344ca7b21fcc202a1
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-29 11:27:09 +0100

    Add web security configurer

---
+++ src/main/java/com/habibInc/issueTracker/security/WebSecurityConfigurer.java
@@ -0,0 +1,8 @@
+package com.habibInc.issueTracker.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+}
