67e8a99e42cd09912568253e9c1d03b55e6ba153
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-30 12:34:37 +0100

    Generate jwt

--- src/main/java/com/habibInc/issueTracker/security/AuthenticationFilter.java
+++ src/main/java/com/habibInc/issueTracker/security/AuthenticationFilter.java
@@ -1,21 +1,31 @@
 package com.habibInc.issueTracker.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    AuthenticationManager authenticationManager;
-    ObjectMapper mapper;
+    private AuthenticationManager authenticationManager;
+    private ObjectMapper mapper;
+
+    @Value("secretKey")
+    private String secretKey;
 
     @Autowired
     public AuthenticationFilter(AuthenticationManager authenticationManager, ObjectMapper mapper) {
@@ -43,5 +53,19 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         } catch (IOException e) {
             throw new RuntimeException("Authentication failed");
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        Jwts.builder()
+                .setSubject(authResult.getName())
+                .setIssuedAt(Date.valueOf(LocalDate.now()))
+                .setExpiration(Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(SignatureAlgorithm.HS256, this.secretKey);
+
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
