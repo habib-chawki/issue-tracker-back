ddc7129c39553f24c21e1bfeee1b73ccbba05cc9
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-30 12:11:41 +0100

    Implement attemptAuthentication filter method

--- src/main/java/com/habibInc/issueTracker/security/AuthenticationFilter.java
+++ src/main/java/com/habibInc/issueTracker/security/AuthenticationFilter.java
@@ -1,6 +1,47 @@
 package com.habibInc.issueTracker.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    AuthenticationManager authenticationManager;
+    ObjectMapper mapper;
+
+    @Autowired
+    public AuthenticationFilter(AuthenticationManager authenticationManager, ObjectMapper mapper) {
+        this.authenticationManager = authenticationManager;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try {
+            // extract the authentication request from the json request body
+            AuthenticationRequest authenticationRequest =
+                    mapper.readValue(request.getInputStream(), AuthenticationRequest.class);
+
+            // create an authentication object with the extracted email and password from the request body
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getEmail(),
+                    authenticationRequest.getPassword()
+            );
+
+            // return the authentication
+            return authenticationManager.authenticate(authentication);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Authentication failed");
+        }
+    }
 }
