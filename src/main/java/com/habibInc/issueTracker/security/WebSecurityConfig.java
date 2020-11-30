eee95f01c54d87814872b902a6908126d62bcfc7
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-30 12:51:20 +0100

    Inject authentication filter into web security config

--- src/main/java/com/habibInc/issueTracker/security/WebSecurityConfig.java
+++ src/main/java/com/habibInc/issueTracker/security/WebSecurityConfig.java
@@ -1,18 +1,40 @@
 package com.habibInc.issueTracker.security;
 
+import com.habibInc.issueTracker.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserService userService;
+    private AuthenticationFilter authenticationFilter;
+
+    @Autowired
+    public WebSecurityConfig(UserService userService, AuthenticationFilter authenticationFilter) {
+        this.userService = userService;
+        this.authenticationFilter = authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable().
-                authorizeRequests().anyRequest().permitAll();
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(authenticationFilter)
+                .authorizeRequests().anyRequest().permitAll();
     }
 
     @Bean
