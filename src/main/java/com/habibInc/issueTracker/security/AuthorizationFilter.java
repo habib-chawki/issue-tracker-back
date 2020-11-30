1bd8f40a267acadf0c22beba72c067827dcf229a
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-30 15:06:30 +0100

    AuthorizationFilter: Intercept the request and check the authorization header

--- src/main/java/com/habibInc/issueTracker/security/AuthorizationFilter.java
+++ src/main/java/com/habibInc/issueTracker/security/AuthorizationFilter.java
@@ -1,5 +1,9 @@
 package com.habibInc.issueTracker.security;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,10 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthorizationFilter extends OncePerRequestFilter {
+    @Value("secretKey")
+    String secretKey;
+
+    // intercept the request and check the Authorization header
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.replace("Bearer ", "");
+
+        String subject = Jwts.parser()
+                                .setSigningKey(secretKey)
+                                .parseClaimsJws(token)
+                                .getBody()
+                                .getSubject();
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(subject, null);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
