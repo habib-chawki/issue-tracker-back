ff969fd2f3f7dcdb399e92e51c49fbb2b474531d
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-01 10:29:13 +0100

    Implement UserDetailsService#loadUserByUsername

--- src/main/java/com/habibInc/issueTracker/security/UserDetailsServiceImpl.java
+++ src/main/java/com/habibInc/issueTracker/security/UserDetailsServiceImpl.java
@@ -1,12 +1,33 @@
 package com.habibInc.issueTracker.security;
 
+import com.habibInc.issueTracker.user.User;
+import com.habibInc.issueTracker.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
+
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String userEmail) {
+        User loadedUser = userRepository.findByEmail(userEmail);
+
+        if(loadedUser != null)
+            return new org.springframework.security.core.userdetails.User(
+                    loadedUser.getEmail(),
+                    loadedUser.getPassword(),
+                    new ArrayList<>());
+
+        throw new UsernameNotFoundException("Incorrect credentials");
     }
 }
