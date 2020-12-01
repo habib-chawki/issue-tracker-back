ad21fa8615ed173922b0b5a2ca27c54367451d0b
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-01 16:04:23 +0100

    Set up athentication filter integration test

--- src/test/java/com/habibInc/issueTracker/security/AuthenticationFilterTest.java
+++ src/test/java/com/habibInc/issueTracker/security/AuthenticationFilterTest.java
@@ -1,20 +1,61 @@
 package com.habibInc.issueTracker.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.habibInc.issueTracker.user.User;
+import com.habibInc.issueTracker.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.boot.test.context.SpringBootTest.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AuthenticationFilter.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AuthenticationFilterTest {
     @Autowired
-    MockMvc mockMvc;
+    UserService userService;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    User user, createdUser;
+    AuthenticationRequest authenticationRequest;
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+
+        user.setFirstName("first");
+        user.setLastName("last");
+        user.setUserName("my_username");
+        user.setEmail("my_email@email.com");
+        user.setPassword("MyPassword");
+
+        createdUser = userService.createUser(user);
+
+        authenticationRequest = new AuthenticationRequest();
+
+        authenticationRequest.setEmail(user.getEmail());
+        authenticationRequest.setPassword(user.getPassword());
+    }
 
     @Test
     public void itShouldLoginUser() throws Exception {
-        mockMvc.perform(post("/users/login")).andExpect(status().isOk());
+        String requestBody = mapper.writeValueAsString(authenticationRequest);
+
+        ResponseEntity res =
+                restTemplate.postForEntity("/users/login", authenticationRequest, Object.class);
+
+        System.out.println(res);
     }
 }
