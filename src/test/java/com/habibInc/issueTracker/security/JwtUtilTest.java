7ac0ef82d70f2defbaf0bcf0e5e0fb24fb7c108c
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-02 21:48:23 +0100

    Test: verify json web token validity

--- src/test/java/com/habibInc/issueTracker/security/JwtUtilTest.java
+++ src/test/java/com/habibInc/issueTracker/security/JwtUtilTest.java
@@ -1,19 +1,39 @@
 package com.habibInc.issueTracker.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(classes = JwtUtil.class)
 public class JwtUtilTest {
     @Autowired
     JwtUtil jwtUtil;
 
+    @Value("${secret.key}")
+    String secretKey;
+
     @Test
     public void itShouldGenerateJsonWebToken() {
         String jwt = jwtUtil.generateToken("Subject");
 
-        Assertions.assertThat(jwt).isNotNull();
+        assertThat(jwt).isNotNull();
+    }
+
+    @Test
+    public void itShouldVerifyJwtAndReturnTheSubject() {
+        String token = Jwts.builder()
+                .setSubject("Habib")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+        String subject = jwtUtil.verifyToken(token);
+
+        assertThat(subject).isEqualTo("Habib");
     }
 }
