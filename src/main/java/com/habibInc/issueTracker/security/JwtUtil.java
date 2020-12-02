764dfb093614c94cc8b7ef50bc05b331f3d3762e
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-02 21:34:40 +0100

    Add generate token method in JwtUtil

---
+++ src/main/java/com/habibInc/issueTracker/security/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.habibInc.issueTracker.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+public class JwtUtil {
+    @Value("secret.key")
+    private String secretKey;
+
+    public String generateToken(String subject) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(Date.valueOf(LocalDate.now()))
+                .setExpiration(Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(SignatureAlgorithm.HS256, this.secretKey.getBytes())
+                .compact();
+    }
+}
