aa3fd132a61198a8ca94bba7936fa8b7bc574809
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-06 11:38:47 +0100

    Annotate user details service with @Component

--- src/main/java/com/habibInc/issueTracker/security/UserDetailsServiceImpl.java
+++ src/main/java/com/habibInc/issueTracker/security/UserDetailsServiceImpl.java
@@ -7,10 +7,12 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
+@Component
 @Primary
 public class UserDetailsServiceImpl implements UserDetailsService {
 
