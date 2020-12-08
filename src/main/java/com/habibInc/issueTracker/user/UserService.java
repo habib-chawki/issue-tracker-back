26084814fb515feacd87c675cec33357f870fa4f
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-08 11:45:20 +0100

    Implement getUserByEmail user service method

--- src/main/java/com/habibInc/issueTracker/user/UserService.java
+++ src/main/java/com/habibInc/issueTracker/user/UserService.java
@@ -29,4 +29,10 @@ public class UserService {
                 () -> new ResourceNotFoundException("User not found")
         );
     }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("User not found")
+        );
+    }
 }
