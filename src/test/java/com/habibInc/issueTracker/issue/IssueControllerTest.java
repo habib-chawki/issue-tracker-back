4fbfa298d95debe37f10d05a00d6d9ea9d3243d9
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-08 17:07:18 +0100

    Mock user service, issue controller unit test

--- src/test/java/com/habibInc/issueTracker/issue/IssueControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/issue/IssueControllerTest.java
@@ -3,6 +3,7 @@ package com.habibInc.issueTracker.issue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
 import com.habibInc.issueTracker.user.User;
+import com.habibInc.issueTracker.user.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,9 @@ public class IssueControllerTest {
 
     @MockBean
     IssueService issueService;
+
+    @MockBean
+    UserService userService;
 
     Issue issue1, issue2;
 
@@ -68,7 +72,7 @@ public class IssueControllerTest {
     public void itShouldCreateIssue() throws Exception {
 
         // mock issue service to add new issue
-        when(issueService.createIssue(issue1, any(User.class))).thenReturn(issue1);
+        when(issueService.createIssue(eq(issue1), any(User.class))).thenReturn(issue1);
 
         // set up json request body
         String requestBody = mapper.writeValueAsString(issue1);
