070938f7c915a83582cd43a7edbf47537f9ff14c
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-09 15:02:59 +0100

    Create comments and set the current logged in user as the owner

--- src/main/java/com/habibInc/issueTracker/comment/CommentService.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentService.java
@@ -3,6 +3,7 @@ package com.habibInc.issueTracker.comment;
 import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
 import com.habibInc.issueTracker.issue.Issue;
 import com.habibInc.issueTracker.issue.IssueService;
+import com.habibInc.issueTracker.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +19,15 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    public Comment createComment(Comment comment, Long issueId) {
+    public Comment createComment(Comment comment, Long issueId, User owner) {
         // delegate call to issueService to get the issue by id
         Issue issue = issueService.getIssue(issueId);
 
         // save the comment if the issue is present
-        if (issue != null)
+        if (issue != null) {
+            comment.setOwner(owner);
             return commentRepository.save(comment);
+        }
 
         throw new ResourceNotFoundException("Issue not found");
     }
