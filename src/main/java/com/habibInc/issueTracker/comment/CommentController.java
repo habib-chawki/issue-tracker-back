15ea11229b72fbe3ae6a7818c3537cb0647cc5cb
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-09 15:02:45 +0100

    Create comments and set the current logged in user as the owner

--- src/main/java/com/habibInc/issueTracker/comment/CommentController.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentController.java
@@ -1,8 +1,10 @@
 package com.habibInc.issueTracker.comment;
 
 import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
+import com.habibInc.issueTracker.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,10 +20,13 @@ public class CommentController {
 
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
-    public Comment createComment(@RequestBody Comment comment, @PathVariable String id){
+    public Comment createComment(@RequestBody Comment comment,
+                                 @PathVariable String id,
+                                 @AuthenticationPrincipal User owner){
         try{
+            // verify issueId type
             Long issueId = Long.parseLong(id);
-            return commentService.createComment(comment, issueId);
+            return commentService.createComment(comment, issueId, owner);
         }catch(NumberFormatException ex){
             throw new InvalidIdException("Invalid issue id");
         }
