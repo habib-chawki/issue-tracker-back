14eb6009aff11924ea4965613e1f12ba33cdb43e
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-12 11:10:46 +0100

    Add forbidden operation exception

---
+++ src/main/java/com/habibInc/issueTracker/exceptionhandler/ForbiddenOperationException.java
@@ -0,0 +1,11 @@
+package com.habibInc.issueTracker.exceptionhandler;
+
+public class ForbiddenOperationException extends RuntimeException {
+    public ForbiddenOperationException(String message) {
+        super(message);
+    }
+
+    public ForbiddenOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
