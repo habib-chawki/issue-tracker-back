b43b04b8960f3c803ae2ce4cf835d6d4e69863cd
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-13 13:15:45 +0100

    Invoke commentService#deleteComment when resource id is valid

--- src/main/java/com/habibInc/issueTracker/comment/CommentController.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentController.java
@@ -39,6 +39,7 @@ public class CommentController {
         try{
             Long parsedCommentId = Long.parseLong(commentId);
             Long parsedIssueId = Long.parseLong(issueId);
+            commentService.deleteComment();
         }catch(NumberFormatException ex){
             throw new InvalidIdException("Invalid id");
         }
