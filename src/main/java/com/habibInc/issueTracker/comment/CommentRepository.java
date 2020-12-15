1f9b86ccaa159f1bfaf3eb1fa0cccbb579925c5a
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-15 12:08:19 +0100

    Remove findCommentByIssueId

--- src/main/java/com/habibInc/issueTracker/comment/CommentRepository.java
+++ src/main/java/com/habibInc/issueTracker/comment/CommentRepository.java
@@ -3,9 +3,6 @@ package com.habibInc.issueTracker.comment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-    Optional<Comment> findByIssueId(Long issueId);
 }
