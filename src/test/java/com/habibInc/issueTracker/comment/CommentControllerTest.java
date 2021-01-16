a68aee0bbb953c60f4cb39e4331219a49d99e22e
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-16 16:22:24 +0100

    Add comment builder

--- src/test/java/com/habibInc/issueTracker/comment/CommentControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/comment/CommentControllerTest.java
@@ -39,12 +39,12 @@ public class CommentControllerTest {
     @BeforeEach
     public void setup() {
         // set up a new comment
-        comment = new Comment();
-
-        comment.setId(1L);
-        comment.setContent("This is a comment");
-        comment.setCreationTime(LocalDateTime.now());
-        comment.setUpdateTime(LocalDateTime.now());
+        comment = Comment.builder()
+                .id(1L)
+                .content("This is a comment")
+                .creationTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
 
         // set the comment issue
         issue = new Issue();
