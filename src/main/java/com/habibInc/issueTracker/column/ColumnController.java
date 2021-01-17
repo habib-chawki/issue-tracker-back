4569822beb1af3ed8e0cbd175dc06e084c0968ad
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 14:09:23 +0100

    Autowire column service

--- src/main/java/com/habibInc/issueTracker/column/ColumnController.java
+++ src/main/java/com/habibInc/issueTracker/column/ColumnController.java
@@ -1,6 +1,7 @@
 package com.habibInc.issueTracker.column;
 
 import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ public class ColumnController {
 
     private ColumnService columnService;
 
+    @Autowired
     public ColumnController(ColumnService columnService) {
         this.columnService = columnService;
     }
