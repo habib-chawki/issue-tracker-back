12ff8a8406d29f5924cfb8ac9154ef33bf4dd5da
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 14:09:06 +0100

    Add create column service method

--- src/main/java/com/habibInc/issueTracker/column/ColumnService.java
+++ src/main/java/com/habibInc/issueTracker/column/ColumnService.java
@@ -1,10 +1,19 @@
 package com.habibInc.issueTracker.column;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ColumnService {
+
+    private ColumnRepository columnRepository;
+
+    @Autowired
+    public ColumnService(ColumnRepository columnRepository) {
+        this.columnRepository = columnRepository;
+    }
+
     public Column createColumn(Column column, Long boardId) {
-        return null;
+        return columnRepository.save(column);
     }
 }
