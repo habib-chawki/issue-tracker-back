881d446ee8ad32f3f89bd1e507e8b155c1072143
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 12:07:09 +0100

    Add column controller

---
+++ src/test/java/com/habibInc/issueTracker/column/ColumnControllerTest.java
@@ -0,0 +1,13 @@
+package com.habibInc.issueTracker.column;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ColumnController.class)
+public class ColumnControllerTest {
+
+    @Test
+    public void itShouldCreateColumn(){
+
+    }
+}
