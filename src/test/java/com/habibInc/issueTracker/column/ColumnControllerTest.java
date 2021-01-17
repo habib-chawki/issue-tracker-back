c3ac96a7cf4f267134ce3c5db015bebcb5f524ff
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 12:18:31 +0100

    Set up create columns test

--- src/test/java/com/habibInc/issueTracker/column/ColumnControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/column/ColumnControllerTest.java
@@ -1,13 +1,27 @@
 package com.habibInc.issueTracker.column;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ColumnController.class)
 public class ColumnControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    Column column;
+
+    @BeforeEach
+    public void setup(){
+        // set up a column
+        column = new Column();
+        column.setName("To do");
+    }
+
     @Test
     public void itShouldCreateColumn(){
-
     }
 }
