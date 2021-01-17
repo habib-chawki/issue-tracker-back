502ef727c7e2c18718f8c7c6a0693f9d8bb8b266
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 12:27:43 +0100

    Test: create columns

--- src/test/java/com/habibInc/issueTracker/column/ColumnControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/column/ColumnControllerTest.java
@@ -1,16 +1,26 @@
 package com.habibInc.issueTracker.column;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(ColumnController.class)
+@WithMockUser
 public class ColumnControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
 
     Column column;
 
@@ -22,6 +32,16 @@ public class ColumnControllerTest {
     }
 
     @Test
-    public void itShouldCreateColumn(){
+    public void itShouldCreateColumn() throws Exception {
+        String url = String.format("/boards/%s/columns", 100L);
+
+        // given the request body
+        String requestBody = mapper.writeValueAsString(column);
+
+        // when a post request is made to create a new column
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isCreated());
     }
 }
