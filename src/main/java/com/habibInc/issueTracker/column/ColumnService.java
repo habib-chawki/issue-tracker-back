0673ec5b256c1552055df159defd605834c1f74f
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 13:03:19 +0100

    Add create column service method

--- src/main/java/com/habibInc/issueTracker/column/ColumnService.java
+++ src/main/java/com/habibInc/issueTracker/column/ColumnService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ColumnService {
-    public Column createColumn(Column column) {
+    public Column createColumn(Column column, Long boardId) {
         return null;
     }
 }
