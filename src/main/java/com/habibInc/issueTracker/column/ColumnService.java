c2c2d17816afe683f2f6a0b7149813ad344de0ef
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 12:48:05 +0100

    Add column service

---
+++ src/main/java/com/habibInc/issueTracker/column/ColumnService.java
@@ -0,0 +1,10 @@
+package com.habibInc.issueTracker.column;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ColumnService {
+    public Column createColumn(Column column) {
+        return null;
+    }
+}
