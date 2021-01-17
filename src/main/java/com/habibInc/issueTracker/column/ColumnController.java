9a37073117d7c7ceb9936ff153841df9e48e05f6
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-17 12:28:17 +0100

    Add column post request handler

--- src/main/java/com/habibInc/issueTracker/column/ColumnController.java
+++ src/main/java/com/habibInc/issueTracker/column/ColumnController.java
@@ -1,4 +1,16 @@
 package com.habibInc.issueTracker.column;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/boards/{boardId}/columns")
+
 public class ColumnController {
+    @PostMapping({"", "/"})
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public Column createColumn(@RequestBody Column column){
+        return null;
+    }
 }
