c24a14438923c963879dfb2b71956cf7f1972ae5
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-18 14:26:39 +0100

    Autowire board service

--- src/main/java/com/habibInc/issueTracker/board/BoardController.java
+++ src/main/java/com/habibInc/issueTracker/board/BoardController.java
@@ -1,5 +1,6 @@
 package com.habibInc.issueTracker.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,10 +8,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/boards")
 public class BoardController {
 
+    private BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService){
+        this.boardService = boardService;
+    }
+
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
     public Board createBoard(@RequestBody Board board){
-        return board;
+        return boardService.createBoard(board);
     }
 
 }
