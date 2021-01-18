6e2f0b7956857b29fe261a33e1fca53812138699
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-18 14:20:18 +0100

    Add board entity

--- src/main/java/com/habibInc/issueTracker/board/Board.java
+++ src/main/java/com/habibInc/issueTracker/board/Board.java
@@ -1,7 +1,24 @@
 package com.habibInc.issueTracker.board;
 
+import lombok.*;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
 }
