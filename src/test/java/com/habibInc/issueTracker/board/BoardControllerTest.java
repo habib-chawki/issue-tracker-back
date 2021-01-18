4714657e23a1b0285cf80c825e396d20042f5bf5
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-18 14:20:34 +0100

    Test: create boards

--- src/test/java/com/habibInc/issueTracker/board/BoardControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/board/BoardControllerTest.java
@@ -1,11 +1,46 @@
 package com.habibInc.issueTracker.board;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @WebMvcTest(BoardController.class)
+@WithMockUser
 public class BoardControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    Board board;
+
+    @BeforeEach
+    public void setup() {
+        board = new Board();
+        board.setName("Scrum");
+    }
+
+    @Test
+    public void itShouldCreateBoard() throws Exception {
+        String url = "/boards";
+
+        String requestBody = mapper.writeValueAsString(board);
+
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(requestBody));
+    }
 }
