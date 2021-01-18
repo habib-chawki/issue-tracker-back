d20a18bcd47e384714470f815ffb5d71c8fd5fbc
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-18 14:26:22 +0100

    Add board service

---
+++ src/main/java/com/habibInc/issueTracker/board/BoardService.java
@@ -0,0 +1,11 @@
+package com.habibInc.issueTracker.board;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    public Board createBoard(Board board){
+        return null;
+    }
+}
