2f4fd679939db073e2465b16aca3e9602599d5d7
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-18 14:20:56 +0100

    Add create board route

--- src/main/java/com/habibInc/issueTracker/board/BoardController.java
+++ src/main/java/com/habibInc/issueTracker/board/BoardController.java
@@ -1,4 +1,16 @@
 package com.habibInc.issueTracker.board;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/boards")
 public class BoardController {
+
+    @PostMapping({"", "/"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public Board createBoard(@RequestBody Board board){
+        return board;
+    }
+
 }
