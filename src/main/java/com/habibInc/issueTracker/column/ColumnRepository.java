3044c8b5fbf7822520dbca492748095dfb86fa0e
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-20 13:36:20 +0100

    Cleanup: remove unused import statements

--- src/main/java/com/habibInc/issueTracker/column/ColumnRepository.java
+++ src/main/java/com/habibInc/issueTracker/column/ColumnRepository.java
@@ -1,13 +1,7 @@
 package com.habibInc.issueTracker.column;
 
-import com.habibInc.issueTracker.issue.Issue;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ColumnRepository extends PagingAndSortingRepository<Column, Long> {
-    List<Issue> findAllIssues(Pageable pageable);
-}
+public interface ColumnRepository extends PagingAndSortingRepository<Column, Long> {}
