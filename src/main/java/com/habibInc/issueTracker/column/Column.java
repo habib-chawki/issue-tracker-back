c36342611a9c772ce4bfc05ff43be15d668fd65e
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-20 16:27:23 +0100

    Add equals and hashCode

--- src/main/java/com/habibInc/issueTracker/column/Column.java
+++ src/main/java/com/habibInc/issueTracker/column/Column.java
@@ -3,6 +3,7 @@ package com.habibInc.issueTracker.column;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 
@@ -20,4 +21,18 @@ public class Column {
     private Long id;
 
     private String title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Column column = (Column) o;
+        return Objects.equals(id, column.id) &&
+                Objects.equals(title, column.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
 }
