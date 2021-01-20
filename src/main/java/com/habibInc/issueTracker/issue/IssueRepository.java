678bf3b998ea7ec0b9b5b7918c4ed54707b23d8b
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-20 13:35:15 +0100

    Find paginated list of issues by column id

--- src/main/java/com/habibInc/issueTracker/issue/IssueRepository.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueRepository.java
@@ -1,7 +1,12 @@
 package com.habibInc.issueTracker.issue;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IssueRepository extends CrudRepository<Issue, Long> {}
+public interface IssueRepository extends PagingAndSortingRepository<Issue, Long> {
+    List<Issue> findByColumnId(Long columnId, Pageable pageable);
+}
