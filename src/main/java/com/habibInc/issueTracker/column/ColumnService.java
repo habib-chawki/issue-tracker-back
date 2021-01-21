5e50ed52b14da0772148e043432708a166a91a00
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-21 12:55:58 +0100

    Create column, fetch board by id and set it before saving

--- src/main/java/com/habibInc/issueTracker/column/ColumnService.java
+++ src/main/java/com/habibInc/issueTracker/column/ColumnService.java
@@ -1,5 +1,7 @@
 package com.habibInc.issueTracker.column;
 
+import com.habibInc.issueTracker.board.Board;
+import com.habibInc.issueTracker.board.BoardService;
 import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
 import com.habibInc.issueTracker.issue.Issue;
 import com.habibInc.issueTracker.issue.IssueRepository;
@@ -15,15 +17,29 @@ public class ColumnService {
 
     private ColumnRepository columnRepository;
     private IssueRepository issueRepository;
+    private BoardService boardService;
 
     @Autowired
-    public ColumnService(ColumnRepository columnRepository, IssueRepository issueRepository) {
+    public ColumnService(ColumnRepository columnRepository, IssueRepository issueRepository, BoardService boardService) {
         this.columnRepository = columnRepository;
         this.issueRepository = issueRepository;
+        this.boardService = boardService;
     }
 
     public Column createColumn(Column column, Long boardId) {
+        // invoke board service to fetch the board by id (throws an exception)
+        Board board = boardService.getBoardById(boardId);
+
+        // set the board
+        column.setBoard(board);
+
+        // save the column
         return columnRepository.save(column);
+    }
+
+    public Column getColumnById(Long boardId, Long columnId) {
+        return columnRepository.findById(columnId)
+                .orElseThrow(() -> new ResourceNotFoundException("Column not found"));
     }
 
     public List<Issue> getPaginatedListOfIssues(Long boardId, Long columnId, int page, int size) {
@@ -31,8 +47,5 @@ public class ColumnService {
         return issueRepository.findByColumnId(columnId, pageable);
     }
 
-    public Column getColumnById(Long boardId, Long columnId) {
-        return columnRepository.findById(columnId)
-                .orElseThrow(() -> new ResourceNotFoundException("Column not found"));
-    }
+
 }
