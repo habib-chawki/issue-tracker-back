1743d371abd4fa032f2910fd7d8e4eae2439e9d8
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-22 18:17:30 +0100

    Json ignore property column

--- src/main/java/com/habibInc/issueTracker/issue/Issue.java
+++ src/main/java/com/habibInc/issueTracker/issue/Issue.java
@@ -1,6 +1,7 @@
 package com.habibInc.issueTracker.issue;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.habibInc.issueTracker.column.Column;
 import com.habibInc.issueTracker.comment.Comment;
@@ -24,6 +25,7 @@ import java.util.Objects;
 
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@JsonIgnoreProperties(value = {"column"})
 public class Issue {
 
     @Id
