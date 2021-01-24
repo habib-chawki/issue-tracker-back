9e36f62ed3a504356bd288790ca2d7f68c8fafe7
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-24 15:28:39 +0100

    Pass authenticated user to board service to create the board

--- src/main/java/com/habibInc/issueTracker/board/BoardController.java
+++ src/main/java/com/habibInc/issueTracker/board/BoardController.java
@@ -1,8 +1,10 @@
 package com.habibInc.issueTracker.board;
 
 import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
+import com.habibInc.issueTracker.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,8 +20,9 @@ public class BoardController {
 
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
-    public Board createBoard(@RequestBody Board board){
-        return boardService.createBoard(board);
+    public Board createBoard(@RequestBody Board board,
+                             @AuthenticationPrincipal User authenticatedUser){
+        return boardService.createBoard(board, authenticatedUser);
     }
 
     @GetMapping("/{boardId}")
