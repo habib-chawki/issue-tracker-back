07f351b24a6583cadee5c560b6a955645ec8f1a5
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-25 16:20:11 +0100

    Refactor: rearange teardown statements

--- src/test/java/com/habibInc/issueTracker/column/ColumnIT.java
+++ src/test/java/com/habibInc/issueTracker/column/ColumnIT.java
@@ -10,7 +10,6 @@ import com.habibInc.issueTracker.security.JwtUtil;
 import com.habibInc.issueTracker.user.User;
 import com.habibInc.issueTracker.user.UserRepository;
 import com.habibInc.issueTracker.user.UserService;
-import org.hibernate.annotations.Columns;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -322,8 +321,9 @@ public class ColumnIT {
 
     @AfterEach
     public void teardown() {
-        userRepository.deleteAll();
         issueRepository.deleteAll();
+        columnRepository.deleteAll();
         boardRepository.deleteAll();
+        userRepository.deleteAll();
     }
 }
