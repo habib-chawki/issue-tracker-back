e124e80070ba26be47dc5511fd0b84e4033d838e
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-13 17:52:32 +0100

    Add the request url to the error body response

--- src/main/java/com/habibInc/issueTracker/exceptionhandler/RestExceptionHandler.java
+++ src/main/java/com/habibInc/issueTracker/exceptionhandler/RestExceptionHandler.java
@@ -7,40 +7,42 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex){
-        ApiError error = new ApiError(ex.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now());
+    public ResponseEntity<ApiError> handleResourceNotFoundException(HttpServletRequest request, ResourceNotFoundException ex) {
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.NOT_FOUND, request.getRequestURI(), LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidIdException.class)
-    public ResponseEntity<ApiError> handleInvalidIdException(InvalidIdException ex){
-        ApiError error = new ApiError(ex.getMessage(), HttpStatus.BAD_REQUEST, LocalDateTime.now());
+    public ResponseEntity<ApiError> handleInvalidIdException(HttpServletRequest request, InvalidIdException ex) {
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.BAD_REQUEST, request.getRequestURI(), LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity<ApiError> handleMalformedJwtException(MalformedJwtException ex) {
-        ApiError error = new ApiError(ex.getMessage(), HttpStatus.UNAUTHORIZED, LocalDateTime.now());
+    public ResponseEntity<ApiError> handleMalformedJwtException(HttpServletRequest request, MalformedJwtException ex) {
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.UNAUTHORIZED, request.getRequestURI(), LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ForbiddenOperationException.class)
-    public ResponseEntity<ApiError> handleUnauthorizedException(ForbiddenOperationException ex){
-        ApiError error = new ApiError(ex.getMessage(), HttpStatus.FORBIDDEN, LocalDateTime.now());
+    public ResponseEntity<ApiError> handleUnauthorizedException(HttpServletRequest request, ForbiddenOperationException ex) {
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.FORBIDDEN, request.getRequestURI(), LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException ex) {
         ApiError error = new ApiError(
                 ex.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
                 HttpStatus.BAD_REQUEST,
+                request.getRequestURI(),
                 LocalDateTime.now()
         );
 
@@ -48,8 +50,8 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleAllOtherExceptions(Exception ex){
-        ApiError error = new ApiError("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());
+    public ResponseEntity<ApiError> handleAllOtherExceptions(HttpServletRequest request, Exception ex) {
+        ApiError error = new ApiError("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR, request.getRequestURI(), LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
