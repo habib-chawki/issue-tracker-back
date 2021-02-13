aada1a759073ef4eb0e8b60ca3de5bbabf3b5c9f
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-13 17:42:13 +0100

    Add http status to error response

--- src/main/java/com/habibInc/issueTracker/exceptionhandler/RestExceptionHandler.java
+++ src/main/java/com/habibInc/issueTracker/exceptionhandler/RestExceptionHandler.java
@@ -14,32 +14,42 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex){
-        ApiError error = new ApiError(ex.getMessage(), LocalDateTime.now());
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidIdException.class)
     public ResponseEntity<ApiError> handleInvalidIdException(InvalidIdException ex){
-        ApiError error = new ApiError(ex.getMessage(), LocalDateTime.now());
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.BAD_REQUEST, LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MalformedJwtException.class)
     public ResponseEntity<ApiError> handleMalformedJwtException(MalformedJwtException ex) {
-        ApiError error = new ApiError(ex.getMessage(), LocalDateTime.now());
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.UNAUTHORIZED, LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ForbiddenOperationException.class)
     public ResponseEntity<ApiError> handleUnauthorizedException(ForbiddenOperationException ex){
-        ApiError error = new ApiError(ex.getMessage(), LocalDateTime.now());
+        ApiError error = new ApiError(ex.getMessage(), HttpStatus.FORBIDDEN, LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        ApiError error = new ApiError(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage(), LocalDateTime.now());
+        ApiError error = new ApiError(
+                ex.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleAllOtherExceptions(Exception ex){
+        ApiError error = new ApiError("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
