4c1fbb207d5575d64de9558eedb1b3a0b9fc1769
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-13 12:08:39 +0100

    Test: login response should contain auth token

--- src/test/java/com/habibInc/issueTracker/security/AuthenticationFilterIT.java
+++ src/test/java/com/habibInc/issueTracker/security/AuthenticationFilterIT.java
@@ -3,14 +3,12 @@ package com.habibInc.issueTracker.security;
 import com.habibInc.issueTracker.user.User;
 import com.habibInc.issueTracker.user.UserRepository;
 import com.habibInc.issueTracker.user.UserService;
-import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -43,20 +41,26 @@ public class AuthenticationFilterIT {
     }
 
     @Test
-    public void itShouldLoginUser() {
+    public void itShouldLogUsersIn() {
+        // given the user is created
         createdUser = userService.createUser(user);
 
+        // given the login request
         authenticationRequest = new AuthenticationRequest(
                 "my_email@email.com", "MyPassword"
         );
 
+        // when the login request is made
         ResponseEntity<String> res = restTemplate.postForEntity(
                 "/login",
                 authenticationRequest,
                 String.class
         );
 
+        // then user login should be successful and the response should contain an auth token
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().containsKey(JwtUtil.HEADER)).isTrue();
+        assertThat(res.getHeaders().get(JwtUtil.HEADER)).isNotNull();
     }
 
     @AfterEach
