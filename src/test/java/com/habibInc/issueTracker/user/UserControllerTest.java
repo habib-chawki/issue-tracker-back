a319b78ee00b162a4d9d13cd07e7e177ddc4a4ff
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-14 15:10:09 +0100

    Fix test: singup users and respond with DTO

--- src/test/java/com/habibInc/issueTracker/user/UserControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
 import com.habibInc.issueTracker.security.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -53,12 +54,16 @@ public class UserControllerTest {
 
         String requestBody = mapper.writeValueAsString(user);
 
+        // given the expected response body
+        UserDto responseBody = new ModelMapper().map(user, UserDto.class);
+
+        // expect the response to be the user DTO
         mockMvc.perform(post("/users/signup")
                 .content(requestBody)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(user.getId().toString()));
+                .andExpect(content().json(mapper.writeValueAsString(responseBody)));
     }
 
     @Test
