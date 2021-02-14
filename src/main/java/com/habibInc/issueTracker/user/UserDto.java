fc669a02749538b06e4e7fed8e25f0b74aaea7e7
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-14 19:59:15 +0100

    Add constructors

--- src/main/java/com/habibInc/issueTracker/user/UserDto.java
+++ src/main/java/com/habibInc/issueTracker/user/UserDto.java
@@ -1,8 +1,12 @@
 package com.habibInc.issueTracker.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 
