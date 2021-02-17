5ce5b16076fc32cf876b3216f412726df38da603
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 20:52:34 +0100

    Add error message

--- src/main/java/com/habibInc/issueTracker/utils/Utils.java
+++ src/main/java/com/habibInc/issueTracker/utils/Utils.java
@@ -3,12 +3,14 @@ package com.habibInc.issueTracker.utils;
 import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
 
 public class Utils {
+    public final static String errorMessage = "Invalid id";
+
     public static Long validateId(String id) {
         Long parsedId;
         try{
             parsedId = Long.valueOf(id);
         }catch (Exception ex){
-            throw new InvalidIdException("Invalid id");
+            throw new InvalidIdException(errorMessage);
         }
         return parsedId;
     }
