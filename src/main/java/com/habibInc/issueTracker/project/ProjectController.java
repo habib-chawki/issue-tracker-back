a01557bf8cc8d76c356e7c64e2a7e97a5207fcb5
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 16:05:10 +0100

    Add project controller

--- src/main/java/com/habibInc/issueTracker/project/ProjectController.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectController.java
@@ -3,13 +3,15 @@ package com.habibInc.issueTracker.project;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
 
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
-    public Project createProject(@RequestBody Project project){
+    public Project createProject(@Valid @RequestBody Project project){
         return project;
     }
 
