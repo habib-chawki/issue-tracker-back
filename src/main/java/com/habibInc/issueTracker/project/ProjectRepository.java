0e1e002c437325d780c3ae018e46ec47d8aeea3a
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 16:08:43 +0100

    Add project repository

---
+++ src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.habibInc.issueTracker.project;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
