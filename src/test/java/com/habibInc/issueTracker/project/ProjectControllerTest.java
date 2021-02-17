cc069d2859ebc3cd03e88585ac009b5ddbfe60b3
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 15:38:02 +0100

    Add project controller setup

--- src/test/java/com/habibInc/issueTracker/project/ProjectControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/project/ProjectControllerTest.java
@@ -1,7 +1,22 @@
 package com.habibInc.issueTracker.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ProjectController.class)
 public class ProjectControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    Project project;
+
+    public void setup() {
+        // set up a project entity
+        project = Project.builder().id(1L).name("Primary proj").build();
+    }
+
+    public void itShouldCreateProject() {
+
+    }
 }
