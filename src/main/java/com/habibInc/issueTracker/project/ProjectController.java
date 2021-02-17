929aa4a39bc8973b6552db19c948118115aa60b3
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 16:02:15 +0100

    Handle POST request, create project

--- src/main/java/com/habibInc/issueTracker/project/ProjectController.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectController.java
@@ -1,4 +1,16 @@
 package com.habibInc.issueTracker.project;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/projects")
 public class ProjectController {
+
+    @PostMapping({"", "/"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project createProject(@RequestBody Project project){
+        return project;
+    }
+
 }
