a6ffd3bde742c3163014fbc7d3e95e022434c1da
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 20:02:53 +0100

    Create project, invoke service

--- src/main/java/com/habibInc/issueTracker/project/ProjectController.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectController.java
@@ -1,18 +1,27 @@
 package com.habibInc.issueTracker.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
 
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
-    public Project createProject(@Valid @RequestBody Project project){
-        return project;
+    public Project createProject(@Valid @RequestBody Project project) {
+        return projectService.createProject(project);
     }
 
 }
