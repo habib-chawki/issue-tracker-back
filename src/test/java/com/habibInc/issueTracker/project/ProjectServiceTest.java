c27599140c426dda7416749a772798d647a749dc
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 16:21:31 +0100

    Test: create project service handler

--- src/test/java/com/habibInc/issueTracker/project/ProjectServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/project/ProjectServiceTest.java
@@ -1,9 +1,14 @@
 package com.habibInc.issueTracker.project;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ProjectServiceTest {
 
@@ -13,13 +18,28 @@ public class ProjectServiceTest {
     @Mock
     ProjectRepository projectRepository;
 
+    Project project;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @BeforeEach
     public void setup() {
-
+        project = Project.builder().id(1L).name("Proj").build();
     }
 
     @Test
     public void itShouldCreateProject() {
+        // given the project repository returns the saved project
+        when(projectRepository.save(project)).thenReturn(project);
 
+        // when the project service is invoked to create the project
+        Project createdProject = projectService.createProject(project);
+
+        // then expect the project repository to have been invoked
+        verify(projectRepository, times(1)).save(project);
+        assertThat(createdProject).isEqualTo(project);
     }
 }
