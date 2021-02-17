c5d87ab63550a0e2f72e5e655db147c190dadcf7
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 15:37:48 +0100

    Add project properties

--- src/main/java/com/habibInc/issueTracker/project/Project.java
+++ src/main/java/com/habibInc/issueTracker/project/Project.java
@@ -1,8 +1,22 @@
 package com.habibInc.issueTracker.project;
 
+import lombok.Builder;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+
+@Builder
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String name;
 
 }
