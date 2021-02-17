09d551145dca9e2e8bc40337dec490ee52a41c33
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 15:27:37 +0100

    Add project controller

--- src/test/java/com/habibInc/issueTracker/project/ProjectControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/project/ProjectControllerTest.java
@@ -1,4 +1,7 @@
 package com.habibInc.issueTracker.project;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ProjectController.class)
 public class ProjectControllerTest {
 }
