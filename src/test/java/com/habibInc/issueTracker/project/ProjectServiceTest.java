0d7d6240a4cf2084d57fd09e4409157839ec9706
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-17 16:30:19 +0100

    Test: project repository, save projects

--- src/test/java/com/habibInc/issueTracker/project/ProjectServiceTest.java
+++ src/test/java/com/habibInc/issueTracker/project/ProjectServiceTest.java
@@ -1,6 +1,5 @@
 package com.habibInc.issueTracker.project;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
