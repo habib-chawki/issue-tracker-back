c2063c64fa90487aac96556888ed9bfb4598a485
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-20 15:22:48 +0100

    Set authenticated user as project owner

--- src/main/java/com/habibInc/issueTracker/project/ProjectService.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectService.java
@@ -3,6 +3,7 @@ package com.habibInc.issueTracker.project;
 import com.habibInc.issueTracker.exceptionhandler.ResourceNotFoundException;
 import com.habibInc.issueTracker.issue.Issue;
 import com.habibInc.issueTracker.issue.IssueRepository;
+import com.habibInc.issueTracker.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,8 @@ public class ProjectService {
         this.issueRepository = issueRepository;
     }
 
-    public Project createProject(Project project) {
+    public Project createProject(Project project, User authenticatedUser) {
+        project.setOwner(authenticatedUser);
         return projectRepository.save(project);
     }
 
