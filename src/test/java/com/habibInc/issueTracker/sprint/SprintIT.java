65fe53f43eeab25a55792eb3c4724b6b6552cc47
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 12:47:31 +0100

    Set up create sprint ITest

--- src/test/java/com/habibInc/issueTracker/sprint/SprintIT.java
+++ src/test/java/com/habibInc/issueTracker/sprint/SprintIT.java
@@ -4,16 +4,20 @@ import com.habibInc.issueTracker.security.JwtUtil;
 import com.habibInc.issueTracker.user.User;
 import com.habibInc.issueTracker.user.UserRepository;
 import com.habibInc.issueTracker.user.UserService;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SprintIT {
+
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Autowired
     UserService userService;
@@ -24,6 +28,7 @@ public class SprintIT {
     @Autowired
     JwtUtil jwtUtil;
 
+    Sprint sprint;
     User authenticatedUser;
     HttpHeaders headers;
 
@@ -42,6 +47,26 @@ public class SprintIT {
         // add Authorization request header
         headers = new HttpHeaders();
         headers.add(JwtUtil.HEADER, JwtUtil.TOKEN_PREFIX + token);
+    }
+
+    @BeforeEach
+    public void setup() {
+        sprint = Sprint.builder()
+                .name("Primary sprint")
+                .goal("Primary goal")
+                .startDate(LocalDateTime.now())
+                .endDate(LocalDateTime.now().plusDays(30))
+                .build();
+    }
+
+    @Nested
+    @DisplayName("POST")
+    class post {
+
+        @Test
+        public void itShouldCreateSprint() {
+
+        }
     }
 
     @AfterAll
