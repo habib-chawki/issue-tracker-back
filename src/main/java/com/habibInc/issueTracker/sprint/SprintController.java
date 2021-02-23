ab5ceb14a01bf99f76f7610871b71db5205fb957
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 12:01:52 +0100

    Invoke sprint service, create sevice

--- src/main/java/com/habibInc/issueTracker/sprint/SprintController.java
+++ src/main/java/com/habibInc/issueTracker/sprint/SprintController.java
@@ -1,5 +1,6 @@
 package com.habibInc.issueTracker.sprint;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,10 +8,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/projects/{projectId}/sprints")
 public class SprintController {
 
+    private final SprintService sprintService;
+
+    @Autowired
+    public SprintController(SprintService sprintService) {
+        this.sprintService = sprintService;
+    }
+
     @PostMapping({"", "/"})
     @ResponseStatus(HttpStatus.CREATED)
     public Sprint createSprint(@RequestBody Sprint sprint) {
-        return sprint;
+        return sprintService.createSprint(sprint);
     }
 
 }
