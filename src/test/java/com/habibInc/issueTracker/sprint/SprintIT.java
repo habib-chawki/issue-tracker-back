903c55feacb69f0c218b432fff5f775f48a68d18
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 13:02:31 +0100

    ITest: create sprint

--- src/test/java/com/habibInc/issueTracker/sprint/SprintIT.java
+++ src/test/java/com/habibInc/issueTracker/sprint/SprintIT.java
@@ -1,16 +1,25 @@
 package com.habibInc.issueTracker.sprint;
 
+import com.habibInc.issueTracker.project.Project;
+import com.habibInc.issueTracker.project.ProjectRepository;
+import com.habibInc.issueTracker.project.ProjectService;
 import com.habibInc.issueTracker.security.JwtUtil;
 import com.habibInc.issueTracker.user.User;
 import com.habibInc.issueTracker.user.UserRepository;
 import com.habibInc.issueTracker.user.UserService;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -26,9 +35,16 @@ public class SprintIT {
     UserRepository userRepository;
 
     @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Autowired
     JwtUtil jwtUtil;
 
     Sprint sprint;
+    Project project;
     User authenticatedUser;
     HttpHeaders headers;
 
@@ -49,11 +65,22 @@ public class SprintIT {
         headers.add(JwtUtil.HEADER, JwtUtil.TOKEN_PREFIX + token);
     }
 
+    @BeforeAll
+    public void globalSetup() {
+        // set up a project
+        project = new Project();
+        project.setName("Project");
+
+        // save the project
+        project = projectService.createProject(project, authenticatedUser);
+    }
+
     @BeforeEach
     public void setup() {
         sprint = Sprint.builder()
                 .name("Primary sprint")
                 .goal("Primary goal")
+                .project(project)
                 .startDate(LocalDateTime.now())
                 .endDate(LocalDateTime.now().plusDays(30))
                 .build();
@@ -63,15 +90,30 @@ public class SprintIT {
     @DisplayName("POST")
     class post {
 
+        private String baseUrl = "/projects/" + project.getId() + "/sprints";
+        private HttpEntity httpEntity;
+
+        @BeforeEach
+        public void setup() {
+            httpEntity = new HttpEntity(sprint, headers);
+        }
+
         @Test
         public void itShouldCreateSprint() {
+            ResponseEntity<Sprint> response =
+                    restTemplate.postForEntity(baseUrl, httpEntity, Sprint.class);
 
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         }
+    }
+
+    @AfterAll
+    public void globalTeardown() {
+        projectRepository.deleteAll();
     }
 
     @AfterAll
     public void authTeardown() {
         userRepository.deleteAll();
     }
-
 }
