0a9a0e41429fa99a17ace7539ab79ff27edd275a
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 11:37:06 +0100

    Add sprint controller test

---
+++ src/test/java/com/habibInc/issueTracker/sprint/SprintControllerTest.java
@@ -0,0 +1,7 @@
+package com.habibInc.issueTracker.sprint;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(SprintController.class)
+public class SprintControllerTest {
+}
