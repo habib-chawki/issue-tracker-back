795d4f441b2928032345322da9358d3a771d48dc
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 12:13:27 +0100

    Create sprint, invoke repository

--- src/main/java/com/habibInc/issueTracker/sprint/SprintService.java
+++ src/main/java/com/habibInc/issueTracker/sprint/SprintService.java
@@ -1,10 +1,19 @@
 package com.habibInc.issueTracker.sprint;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SprintService {
+
+    private final SprintRepository sprintRepository;
+
+    @Autowired
+    public SprintService(SprintRepository sprintRepository) {
+        this.sprintRepository = sprintRepository;
+    }
+
     public Sprint createSprint(Sprint sprint) {
-        return null;
+        return sprintRepository.save(sprint);
     }
 }
