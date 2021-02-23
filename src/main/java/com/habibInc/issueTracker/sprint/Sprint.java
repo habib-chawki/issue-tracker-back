3279d586a76b6be3d5930ff3eac2b0943706766c
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 13:58:06 +0100

    Add issues

--- src/main/java/com/habibInc/issueTracker/sprint/Sprint.java
+++ src/main/java/com/habibInc/issueTracker/sprint/Sprint.java
@@ -1,10 +1,13 @@
 package com.habibInc.issueTracker.sprint;
 
+import com.habibInc.issueTracker.issue.Issue;
 import com.habibInc.issueTracker.project.Project;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 
@@ -28,4 +31,20 @@ public class Sprint {
 
     @ManyToOne
     private Project project;
+
+    @OneToMany(mappedBy = "sprint")
+    private List<Issue> issues;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sprint sprint = (Sprint) o;
+        return id.equals(sprint.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
