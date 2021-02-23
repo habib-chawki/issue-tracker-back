1db85b6db04d6949333c36fa6602ca2a7e1a7b81
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 11:43:24 +0100

    Set up sprint entity

--- src/main/java/com/habibInc/issueTracker/sprint/Sprint.java
+++ src/main/java/com/habibInc/issueTracker/sprint/Sprint.java
@@ -1,7 +1,25 @@
 package com.habibInc.issueTracker.sprint;
 
+import lombok.Builder;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
+
+@Builder
+
 public class Sprint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String goal;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 }
--- src/test/java/com/habibInc/issueTracker/sprint/SprintControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/sprint/SprintControllerTest.java
@@ -1,7 +1,22 @@
 package com.habibInc.issueTracker.sprint;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import java.time.LocalDateTime;
 
 @WebMvcTest(SprintController.class)
 public class SprintControllerTest {
+
+    Sprint sprint;
+
+    @BeforeEach
+    public void setup(){
+        sprint = Sprint.builder()
+                .id(1L)
+                .name("First sprint")
+                .goal("sprint goal")
+                .startDate(LocalDateTime.now())
+                .endDate(LocalDateTime.now().plusMonths(1)).build();
+    }
 }
