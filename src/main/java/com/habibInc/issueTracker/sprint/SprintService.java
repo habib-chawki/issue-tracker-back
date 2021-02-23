69b54f5d5ae78c5439b30bd71bb0b7d4c5311c3c
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-23 11:54:15 +0100

    Add sprint service

---
+++ src/main/java/com/habibInc/issueTracker/sprint/SprintService.java
@@ -0,0 +1,7 @@
+package com.habibInc.issueTracker.sprint;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SprintService {
+}
--- src/test/java/com/habibInc/issueTracker/sprint/SprintControllerTest.java
+++ src/test/java/com/habibInc/issueTracker/sprint/SprintControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,9 @@ public class SprintControllerTest {
 
     @Autowired
     ObjectMapper mapper;
+
+    @MockBean
+            SprintService sprintService;
 
     Sprint sprint;
 
