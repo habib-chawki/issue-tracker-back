c06f1cd5c333e974468bef2b5f268d5871a40209
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-27 15:30:49 +0100

    Update issue column

--- src/main/java/com/habibInc/issueTracker/issue/IssueRepository.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueRepository.java
@@ -1,13 +1,21 @@
 package com.habibInc.issueTracker.issue;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface IssueRepository extends PagingAndSortingRepository<Issue, Long> {
     List<Issue> findByColumnId(Long columnId, Pageable pageable);
     List<Issue> findAllByProjectId(Long projectId);
+
+    @Modifying
+    @Query(value = "UPDATE issues SET column_id = :columnId WHERE issue_id = :issueId", nativeQuery = true)
+    int updateIssueColumn(@Param("issueId") Long issueId, @Param("columnId") Long columnId);
 }
