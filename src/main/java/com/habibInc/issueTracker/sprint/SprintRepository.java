5ccc201d677d28569ab8bb51720eeadac05acbcc
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-13 12:11:25 +0100

    Find list of sprints by status

--- src/main/java/com/habibInc/issueTracker/sprint/SprintRepository.java
+++ src/main/java/com/habibInc/issueTracker/sprint/SprintRepository.java
@@ -3,6 +3,9 @@ package com.habibInc.issueTracker.sprint;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SprintRepository extends CrudRepository<Sprint, Long> {
+    List<Sprint> findAllByStatus(SprintStatus status);
 }
