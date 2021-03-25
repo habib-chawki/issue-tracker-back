d7fc10e71d6fb38869b9f70934151c422eb4a4df
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-25 12:36:51 +0100

    Remove teardown in @DataJpaTest

--- src/test/java/com/habibInc/issueTracker/user/UserRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/user/UserRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.habibInc.issueTracker.user;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
