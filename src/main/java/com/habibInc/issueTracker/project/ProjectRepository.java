283fe3bf93462982671646982e9c79f5ddee0a6f
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-25 14:35:20 +0100

    Find all projects by assinged user id

--- src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.habibInc.issueTracker.project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Set<Project> findAllByAssignedUsersId(Long userId);
 }
