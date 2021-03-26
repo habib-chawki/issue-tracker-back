06f7d820eb1067943f3f3e7bc5cb33052e897b1d
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-26 15:12:45 +0100

    Add @Transactional

--- src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -12,10 +13,9 @@ import java.util.Set;
 public interface ProjectRepository extends CrudRepository<Project, Long> {
     Set<Project> findAllByAssignedUsersId(Long userId);
 
-    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query(value = "INSERT INTO project_user(user_id, project_id) VALUES(:userId, :projectId) ", nativeQuery = true)
     void addUserToProject(@Param(value = "userId") Long userId,
                           @Param(value = "projectId") Long projectId);
-
-
 }
