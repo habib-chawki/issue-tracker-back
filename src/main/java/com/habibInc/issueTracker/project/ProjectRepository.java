30516002f3dc24ba4946db16c2667ed4509c128c
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-26 13:34:50 +0100

    Add user to project

--- src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
+++ src/main/java/com/habibInc/issueTracker/project/ProjectRepository.java
@@ -1,6 +1,9 @@
 package com.habibInc.issueTracker.project;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
@@ -8,4 +11,11 @@ import java.util.Set;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
     Set<Project> findAllByAssignedUsersId(Long userId);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "INSERT INTO project_user(user_id, project_id) VALUES(:userId, :projectId) ", nativeQuery = true)
+    void addUserToProject(@Param(value = "userId") Long userId,
+                          @Param(value = "projectId") Long projectId);
+
+
 }
