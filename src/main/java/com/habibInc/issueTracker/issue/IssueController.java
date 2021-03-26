e3c8efb03883609e9c64c9797a1188abee1a4a04
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-26 11:33:45 +0100

    Update issue assignee, return updated issue DTO

--- src/main/java/com/habibInc/issueTracker/issue/IssueController.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueController.java
@@ -1,22 +1,31 @@
 package com.habibInc.issueTracker.issue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
 import com.habibInc.issueTracker.user.User;
-import com.habibInc.issueTracker.user.UserService;
+import com.habibInc.issueTracker.utils.Utils;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/issues")
 public class IssueController {
 
     private final IssueService issueService;
+    private final ModelMapper modelMapper;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public IssueController(IssueService issueService, UserService userService) {
+    public IssueController(IssueService issueService, ModelMapper modelMapper, ObjectMapper objectMapper) {
         this.issueService = issueService;
+        this.modelMapper = modelMapper;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping(value = {"", "/"}, params = "project")
@@ -69,7 +78,18 @@ public class IssueController {
 
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void updateIssueAssignee(){
+    public IssueDto updateIssueAssignee(@PathVariable("id") Long issueId,
+                                        @RequestBody String request) throws JsonProcessingException {
+        // extract the new assignee id from the request body
+        Map<String, String> map = objectMapper.readValue(request, Map.class);
+        Long assigneeId = Utils.validateId(map.get("assignee"));
 
+        // invoke service, update assignee
+        Issue issue = issueService.updateIssueAssignee(issueId, assigneeId);
+
+        // convert to IssueDto
+        IssueDto updatedIssue = modelMapper.map(issue, IssueDto.class);
+
+        return updatedIssue;
     }
 }
