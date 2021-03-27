6ae5872a769b6ba27f5281286e28a54a98c659e4
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-27 21:34:15 +0100

    Find all users by page

--- src/main/java/com/habibInc/issueTracker/user/UserRepository.java
+++ src/main/java/com/habibInc/issueTracker/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.habibInc.issueTracker.user;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Set<User> findAllByAssignedProjectsId(Long projectId);
+
+    List<User> findAll(Pageable pageable);
 }
