9b68f7d7ad272770165f0b19966d85f0aef49ab9
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-12 17:14:06 +0200

    Test with H2 in-memory database

--- src/test/java/com/habibInc/issueTracker/project/ProjectRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/project/ProjectRepositoryTest.java
@@ -6,18 +6,14 @@ import com.habibInc.issueTracker.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class ProjectRepositoryTest {
 
     @Autowired
