7217cddda3b990d37d6d257017577573de194fc5
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-12 17:13:46 +0200

    Test with H2 in-memory database

--- src/test/java/com/habibInc/issueTracker/comment/CommentRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/comment/CommentRepositoryTest.java
@@ -4,17 +4,14 @@ import com.habibInc.issueTracker.issue.IssueRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class CommentRepositoryTest {
 
     @Autowired
