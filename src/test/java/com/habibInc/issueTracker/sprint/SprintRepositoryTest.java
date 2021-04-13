0067369bdf2ef58e49f3dcba0d880b087c5e119f
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-13 14:52:35 +0200

    Don't replace the application default DataSource

--- src/test/java/com/habibInc/issueTracker/sprint/SprintRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/sprint/SprintRepositoryTest.java
@@ -5,6 +5,7 @@ import com.habibInc.issueTracker.issue.IssueRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class SprintRepositoryTest {
 
     @Autowired
