b5327b3a08e11fa41bf0d0e4c88e66aaeb4c82c4
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-13 14:51:53 +0200

    Don't replace the application default DataSource

--- src/test/java/com/habibInc/issueTracker/column/ColumnRepositoryTest.java
+++ src/test/java/com/habibInc/issueTracker/column/ColumnRepositoryTest.java
@@ -3,6 +3,7 @@ package com.habibInc.issueTracker.column;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class ColumnRepositoryTest {
     @Autowired
     ColumnRepository columnRepository;
