86e9cfb1ad8bb5dfff4a84c2a01b0646154c149c
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-24 16:08:32 +0200

    Ignore issue assingee

--- src/main/java/com/habibInc/issueTracker/board/BoardDto.java
+++ src/main/java/com/habibInc/issueTracker/board/BoardDto.java
@@ -1,7 +1,6 @@
 package com.habibInc.issueTracker.board;
 
 import com.habibInc.issueTracker.column.Column;
-import com.habibInc.issueTracker.sprint.SprintBacklogDto;
 import com.habibInc.issueTracker.user.UserDto;
 import lombok.Data;
 
@@ -14,7 +13,6 @@ public class BoardDto {
     private String name;
     private List<Column> columns;
     private UserDto owner;
-    private SprintBacklogDto sprint;
 
     @Override
     public boolean equals(Object o) {
