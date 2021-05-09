4509e013b7b7ab363b9a6512ea76d1743d2b6058
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-09 16:30:20 +0200

    Remove assignee from ignored json properties

--- src/main/java/com/habibInc/issueTracker/issue/Issue.java
+++ src/main/java/com/habibInc/issueTracker/issue/Issue.java
@@ -25,7 +25,7 @@ import java.util.Objects;
 @Builder
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-@JsonIgnoreProperties(value = {"column", "reporter", "assignee", "sprint", "project"})
+@JsonIgnoreProperties(value = {"column", "reporter", "sprint", "project"})
 @Table(name = "issue")
 public class Issue {
 
@@ -53,7 +53,7 @@ public class Issue {
 
     private int votes;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.REFRESH)
     private User assignee;
 
     @OneToOne
