e4cf71809ce6120d9cd8a8c39f40535d5bc936c7
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-11 18:13:10 +0200

    Set comments dto list

--- src/main/java/com/habibInc/issueTracker/issue/IssueDto.java
+++ src/main/java/com/habibInc/issueTracker/issue/IssueDto.java
@@ -1,10 +1,13 @@
 package com.habibInc.issueTracker.issue;
 
+import com.habibInc.issueTracker.comment.CommentDto;
 import com.habibInc.issueTracker.user.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,6 +26,7 @@ public class IssueDto {
     private IssuePriority priority;
 
     private int points;
+    private List<CommentDto> comments;
 
     private UserDto assignee;
     private UserDto reporter;
