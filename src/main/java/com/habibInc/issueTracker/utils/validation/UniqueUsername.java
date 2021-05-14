aeec90182e619afab9fe3481cd152cfacaeaaf72
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-14 19:05:38 +0200

    Add unique username annotation

---
+++ src/main/java/com/habibInc/issueTracker/utils/validation/UniqueUsername.java
@@ -0,0 +1,21 @@
+package com.habibInc.issueTracker.utils.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = UniqueUsernameValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueUsername {
+
+    String message() default "Username is not unique";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
