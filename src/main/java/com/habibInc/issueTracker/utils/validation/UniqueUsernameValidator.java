9df4967c390be639acf092849d3546d1967b10f5
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-14 19:06:03 +0200

    Validate unique username

---
+++ src/main/java/com/habibInc/issueTracker/utils/validation/UniqueUsernameValidator.java
@@ -0,0 +1,17 @@
+package com.habibInc.issueTracker.utils.validation;
+
+import com.habibInc.issueTracker.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
+        return userRepository.existsByUsername(username);
+    }
+}
