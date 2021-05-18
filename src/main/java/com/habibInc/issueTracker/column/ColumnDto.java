f708848caad5fb12ac84e5ba5e6537a092c850d1
habib-chawki <touati.habib.chawki@gmail.com>
2021-05-18 14:10:18 +0200

    Add column DTO

---
+++ src/main/java/com/habibInc/issueTracker/column/ColumnDto.java
@@ -0,0 +1,39 @@
+package com.habibInc.issueTracker.column;
+
+import com.habibInc.issueTracker.board.BoardDto;
+import com.habibInc.issueTracker.issue.IssueDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class ColumnDto {
+
+    private Long id;
+    private String title;
+
+    private BoardDto board;
+
+    private List<IssueDto> issues;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ColumnDto columnDto = (ColumnDto) o;
+        return id.equals(columnDto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
