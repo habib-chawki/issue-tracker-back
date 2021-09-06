f589066fe4256a06c3c0b90e2546f54e8a10a6f9
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-06 15:24:34 +0200

    Add toString()

--- src/main/java/com/habibInc/issueTracker/board/BoardDto.java
+++ src/main/java/com/habibInc/issueTracker/board/BoardDto.java
@@ -23,6 +23,16 @@ public class BoardDto {
     private Long ownerId;
 
     @Override
+    public String toString() {
+        return "{" +
+                "id: " + id +
+                ", name: '" + name + '\'' +
+                ", columns: " + columns +
+                ", owner: " + ownerId +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
