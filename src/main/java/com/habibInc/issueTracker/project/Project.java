beeda555468a9c4b96b08c6b1c5b1939a5079885
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-06 15:13:22 +0200

    Add toString()

--- src/main/java/com/habibInc/issueTracker/project/Project.java
+++ src/main/java/com/habibInc/issueTracker/project/Project.java
@@ -47,6 +47,14 @@ public class Project {
     private Set<User> assignedUsers;
 
     @Override
+    public String toString() {
+        return "{" +
+                "id: " + id +
+                ", name: '" + name + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
