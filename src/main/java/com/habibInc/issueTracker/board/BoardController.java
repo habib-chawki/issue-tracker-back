ded1ba8ee2216375eed1a4b1aa07ddde905bfc4d
habib-chawki <touati.habib.chawki@gmail.com>
2021-09-06 15:24:47 +0200

    Log: board controller

--- src/main/java/com/habibInc/issueTracker/board/BoardController.java
+++ src/main/java/com/habibInc/issueTracker/board/BoardController.java
@@ -2,12 +2,14 @@ package com.habibInc.issueTracker.board;
 
 import com.habibInc.issueTracker.exceptionhandler.InvalidIdException;
 import com.habibInc.issueTracker.user.User;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
@@ -29,7 +31,11 @@ public class BoardController {
         Board createdBoard = boardService.createBoard(sprintId, board, authenticatedUser);
 
         // map board DTO
-        return modelMapper.map(createdBoard, BoardDto.class);
+        final BoardDto createdBoardDto = modelMapper.map(createdBoard, BoardDto.class);
+
+        log.info("Created board: {}", createdBoardDto);
+
+        return createdBoardDto;
     }
 
     @GetMapping("/{boardId}")
@@ -37,7 +43,10 @@ public class BoardController {
     public Board getBoard(@PathVariable("boardId") String id) {
         try{
             Long boardId = Long.parseLong(id);
-            return boardService.getBoardById(boardId);
+            final Board boardById = boardService.getBoardById(boardId);
+
+            log.info("Fetched board by id: {boardId: {}}", id);
+            return boardById;
         }catch(NumberFormatException ex){
             throw new InvalidIdException("Invalid board id");
         }
@@ -50,6 +59,7 @@ public class BoardController {
         try{
             Long boardId = Long.parseLong(id);
             boardService.deleteBoardById(boardId, authenticatedUser);
+            log.info("Delete board by id: {boardId: {}, owner: {}}", id, authenticatedUser.getId());
         }catch (NumberFormatException ex){
             throw new InvalidIdException("Invalid board id");
         }
